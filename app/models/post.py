028a1b16e2e83d48737211398b1ecb73d83e6cef
fasika <fasikafikadu38@gmail.com>
2024-06-07 13:01:42 +0300

    feat: add db models and pydnatic schemas

---
+++ app/models/post.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from app.db.base import Base
+
+class Post(Base):
+    __tablename__ = "posts"
+
+    id = Column(Integer, primary_key=True, index=True)
+    text = Column(String, nullable=False)
+    owner_id = Column(Integer, ForeignKey("users.id"))
+    owner = relationship("User", back_populates="posts")
